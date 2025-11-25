4905397062e7629c453d8c14f0a77d333120cfaa
obrioxia <j.obrien@obrioxia.com>
2025-11-25 21:35:57 +0000

    Update API Key in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-  // 1. URL must be exactly this (no trailing slash)
   apiUrl: 'https://obrioxia-backend-pkrp.onrender.com', 
-  
-  // 2. Key must match the one in Render Dashboard
   apiKey: 'c919848182e3e4250082ea7bacd14e170' 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: false,
-  // Base URL only (Service adds /api/...)
   apiUrl: 'https://obrioxia-backend-pkrp.onrender.com', 
-  
-  // Matches the API Key in your Render Dashboard
   apiKey: 'c919848182e3e4250082ea7bacd14e170' 
 };
